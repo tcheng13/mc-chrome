4014eb831c280853053fd43d1a28ab149981e15a
Tim Cheng <tcheng13@berkeley.edu>
2020-01-22 11:43:57 -0500

    added npm modules for jquery

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 var div=document.createElement("div"); 
+div.innerHTML = "<form class='form'><input type='text' placeholder='Email' required> <div class='container'><input type='submit' value='Subscribe'></div></form>"
 document.body.appendChild(div); 
-div.innerText="test123";
-alert("hellooooo")
