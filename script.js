4e8ee2f16ef67b2dfa233f3accc59fafb9ffe25f
Tim Cheng <tcheng13@berkeley.edu>
2020-02-20 11:35:05 -0500

    merge issue fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     var div=document.createElement("div"); 
-    div.innerHTML ='<div class="mc_embed_signup"><form action="https://mailchimp.us4.list-manage.com/subscribe/post-json?u=815e5f55b60327dbc95cc0f36&amp;id=56c531af63&c=?" method="GET" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate><div id="mc_embed_signup_scroll"><input type="email" value="" placeholder="Email address" name="EMAIL" class="required email" id="mce-EMAIL"><div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_815e5f55b60327dbc95cc0f36_56c531af63" tabindex="-1" value=""></div><div class="clear"><input type="submit" value="Subscribe now" name="subscribe" id="mc-embedded-subscribe" class="button"></div></div></form></div><div class="success-message"><svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52" width="70"><circle class="checkmark-circle" cx="26" cy="26" r="23" fill="none"/><path class="checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/> </svg><div><h4>Success!</h4></div></div>'
+    div.innerHTML = htmlToRender;
     document.body.appendChild(div); 
 
     var allCookies = document.cookie;
@@ -8,7 +8,7 @@ $(document).ready(function() {
     for(var i=0; i<cookiearray.length; i++) {
       name = cookiearray[i].split('=')[0].toString();
       if (name == " emailadd") {
-        $(".mc_embed_signup").hide()
+        // $(".mc_embed_signup").hide()
       }
     }
 
@@ -77,5 +77,44 @@ $(document).ready(function() {
       return;
     });
   });
+
+  const pencilImage = chrome.extension.getURL('./pictures/signup-pencil.png');
+
+  const htmlToRender =
+
+  `<div class="mc_embed_signup">
+    <figure class="subscription_container_image">
+      <img src="${pencilImage}" alt="Signup Pencil">
+    </figure>
+    <h2 class="h2 margin">
+      Want What's In Store in your inbox? Sign up below.
+    </h2>
+    <form action="https://mailchimp.us4.list-manage.com/subscribe/post-json?u=815e5f55b60327dbc95cc0f36&amp;id=56c531af63&c=?" method="GET" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+      <div id="mc_embed_signup_scroll">
+        <input type="email" value="" placeholder="Email address" name="EMAIL" class="required email" id="mce-EMAIL"><div style="position: absolute; left: -5000px;" aria-hidden="true">
+      </div>
+      <div class="clear">
+        <input type="submit" value="Subscribe now" name="subscribe" id="mc-embedded-subscribe" class="button">
+      </div>
+      <div class="feedback feedback--success feedback--icon">
+        <div class="feedback__content">
+          <span>
+            Thank you for subscribing!
+            <a href="https://mailchimp.us12.list-manage.com/profile?u=9c59d08468a2cd3275953b3f6&id=33043de115&e=2848b82323">
+            Click here to manage your preferences</a>
+          </span>
+        </div>
+      </div>
+    </form>
+    <div class="info">
+      <p>
+          Your information will be used to send you Mailchimp updates. 
+          You can change your mind at any time by clicking the unsubscribe 
+          link at the bottom of emails you receive from us. For more details, 
+          review our privacy policy.
+          <a href="/legal/privacy"> privacy policy</a>
+      </p>    
+    </div>
+  </div>`;
 
 
